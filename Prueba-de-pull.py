e0f3a593846c78f022fe7005ece56eb216e536a4
Pardosito <108704303+Pardosito@users.noreply.github.com>
2023-09-20 14:33:28 -0600

    Create Prueba-de-pull.py

---
+++ Prueba-de-pull.py
@@ -0,0 +1,7 @@
+sueldo = eval(input("¿Cuál es el sueldo del trabajador? "))
+
+if sueldo < 10000:
+    sueldo *= 1.15 #sueldo = sueldo * 1.15
+    print(sueldo)
+
+print("Cálculo de sueldo terminado")
